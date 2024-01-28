a0910bfecb561e9c88b9543f22a1147ec86f0415
Oskar Johansson <oskar.tzar@hotmail.com>
2024-01-28 15:39:16 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Labb_3___Anropa_databasen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
